a841e8d4d9adc43cfeb9452df359a45a8053650a
gerimujo <geri.mujo@softup.co>
2024-09-09 16:53:45 +0200

    created user and auth modules

---
+++ constants/controllerPath.ts
@@ -0,0 +1,4 @@
+export const controller_path = {
+  USER: { GET_ONE: ':id', PATCH_ONE: ':id', DELETE_ONE: ':id', GET_ALL: '' },
+  AUTH: { SIGN_UP: 'sign_up', SIGN_IN: 'sign_in', SIGN_OUT: 'sign_out' },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UserModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
